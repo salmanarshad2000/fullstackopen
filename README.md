6c5fa5b33cde09dee42df5adefdaa4242844c824
Salman Arshad <salmanarshad2000@outlook.com>
2023-08-14 03:36:26 +0500

    part 1, unicafe app

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # fullstackopen
 
 My submissions for UH Full Stack Open 2023
-
-## Naming Scheme
-
-```
-part0
-  ex-4.md
-  ex-5.md
-  ex-6-md
-part1
-  ...
-```
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (total === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  } else {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={(good - bad) / total} />
+          <StatisticLine text="positive" value={100 * good / total} />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <p>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </p>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
