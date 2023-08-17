36f9c830ca157495d2d8a08c8af21a5ffdb63ec1
Salman Arshad <salmanarshad2000@outlook.com>
2023-08-17 13:21:01 +0500

    part 2, courseinfo app

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -10,11 +10,17 @@ const Part = ({ part }) => {
   return (<p>{part.name} {part.exercises}</p>)
 }
 
+const Summary = ({ parts }) => {
+  const total = parts.reduce((total, part) => total + part.exercises, 0)
+  return (<p><b>total of {total} exercises</b></p>)
+}
+
 const Course = ({ course }) => {
   return (
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <Summary parts={course.parts} />
     </div>
   )
 }
