3cea718e82886a8b6fafb341d0bcccb608e45b76
Salman Arshad <salmanarshad2000@users.noreply.github.com>
2023-08-30 19:35:40 +0500

    Migrated all apps from create-react-app to Vite

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
