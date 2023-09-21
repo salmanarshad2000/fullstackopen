db889a840a6e608231cb652ec1205cf146038f9e
Salman Arshad <salmanarshad2000@users.noreply.github.com>
2023-09-21 07:01:11 -0700

    part 3, until exercise 3.3

---
+++ part3/phonebook-api/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person === undefined) {
+    response.status(404).json({ error: "person not found" })
+    return
+  }
+  response.json(person)
+})
+
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length}<br>${new Date()}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
